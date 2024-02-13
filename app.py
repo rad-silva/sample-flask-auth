3bb806d36ec8b47af934385f8be8f9a9eb1ed49c
ricardo <radsilva@outlook.com.br>
2024-02-13 11:04:01 -0300

    criação da lógica de login/logout

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "YOUR_SECRET_KEY"
@@ -9,17 +9,47 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 login_manager = LoginManager()
 db.init_app(app)
-login_manager.init_app()
+login_manager.init_app(app)
 
-@app.route('/login', methods=["POST"])
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+@app.route('/login', methods=['POST'])
 def login():
     data = request.json
     username = data.get("username")
     password = data.get("password")
     
     if username and password:
-        x=3
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.password == password:
+            login_user(user)
+            print(current_user.is_authenticated)
+            return jsonify({"message": "Autenticação realizada com sucesso"}), 200
+
     return jsonify({"message": "Credênciais inválidas"}), 400
+
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Logout realizado com sucesso"}), 200
+
+@app.route('/user', methods=['POST'])
+def create_user():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+    
+    if username and password:
+        return jsonify({"message": "Usuário cadastrado com sucesso"}), 200
+    
+    return jsonify({"message": "Dados inválidos"}), 401
+        
 
 @app.route("/hello", methods=["GET"])
 def hello():
